1b559a5cad0b86f668748cb3ceb30540d1083857
kyoungsjjj0211 <kyoungsjjj0513@gmail.com>
2025-08-28 18:15:13 +0900

    day004

--- track003_java/java2025/src/com/company/java002_ex/VarEx001.java
+++ track003_java/java2025/src/com/company/java002_ex/VarEx001.java
@@ -2,24 +2,21 @@ package com.company.java002_ex;
 
 public class VarEx001 {
 	public static void main(String[] args) {
-		int apple = 0;
-		apple = 1000;
-		System.out.println("사과가격은 " +apple+"원입니다");
-	
-		apple = apple+1000;
 		
-		//apple=2000;
-		//System.out.printf("사과가격은 $d원입니다" , apple);
-	System.out.println("사과가격은 "+apple+ "원입니다");
-	
 	}
 }
-
-
-//1-1.  apple라는 변수만들기
-//1-2.  1-1.에서 만든 변수에 1000이라는 데이터 대입하시오.  ( 자료형 int )
-//1-3.  1-1.에서 만든 변수이용해서 
-//        사과가격은 1000원입니다 출력
-//1-4.  1-1.에서 만든 변수에 2000이라는 데이터 대입하시오.  
-//1-5.  1-1.에서 만든 변수이용해서 
-//사과가격은 2000원입니다 출력
+/*
+연습문제2)  
+패키지명 : com.company.java002_ex
+클래스명 : VarEx002
+출력내용 : 
+   1-1.  정수형데이터를 담을수 있는 변수 a 만들고   
+   1-2.  a에 10대입하시오
+   1-3.  정수형데이터를 담을수 있는 변수 b 만들고   
+   1-4.  b에 3대입하시오
+   1-5.  System.out.println 을 4번사용해서 
+     10 + 3 = 13
+     10  - 3 = 7
+     10  * 3 = 30
+     10  / 3 = 3 
+*/
