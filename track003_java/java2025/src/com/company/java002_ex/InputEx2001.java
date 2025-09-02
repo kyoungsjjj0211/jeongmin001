d64521f8dc1cacd139217ea07a3d23f66fc9fa2b
kyoungsjjj0211 <kyoungsjjj0513@gmail.com>
2025-09-02 17:00:45 +0900

    java백업

---
+++ track003_java/java2025/src/com/company/java002_ex/InputEx2001.java
@@ -0,0 +1,16 @@
+package com.company.java002_ex;
+
+import java.util.Scanner;
+
+public class InputEx2001 {
+	public static void main(String[] args) {
+		double d = 3.14;
+		Scanner scanner = new Scanner(System.in) ;
+		System.out.print("파이값을 입력하시오 >");
+		double 파이 = scanner.nextDouble();
+		System.out.print("파이값은" + 파이 + "입니다.");
+			
+	}
+	
+
+}
