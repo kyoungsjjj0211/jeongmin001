465eec730353b8b15c221bdf3bb97dfdfd2c093f
kyoungsjjj0211 <kyoungsjjj0513@gmail.com>
2025-09-19 12:50:17 +0900

    day020

---
+++ track003_java/java2025/src/com/company/java008_Ex/Repeat4.java
@@ -0,0 +1,12 @@
+package com.company.java008_Ex;
+
+public class Repeat4 {
+	public static void disp(int num, char ch) {
+		for(int i=0; i<num;i++) {System.out.print(ch);}
+	}
+	public static void main(String[] args) {
+	disp(7,'*');
+
+	}
+
+}
