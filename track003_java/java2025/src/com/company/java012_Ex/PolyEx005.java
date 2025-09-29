5e55db1bd4a39b5a394696068da015266316b8e0
kyoungsjjj0211 <kyoungsjjj0513@gmail.com>
2025-09-29 18:02:44 +0900

    back

--- track003_java/java2025/src/com/company/java012_Ex/PolyEx005.java
+++ track003_java/java2025/src/com/company/java012_Ex/PolyEx005.java
@@ -1,5 +1,15 @@
 package com.company.java012_Ex;
-
+/*
+Object
+  ↑
+Parent7  int x=100, method() -> "Parent Method"
+  ↑
+Child7 int x= 200, method() -> "ChildMethod"
+*/
+//Q1. 상속도 그리기
+//Q2. 각클래스에서 사용할수있는 멤버변수/멤버메서드
+//A. Parent7 x=100   method() -> "Parent Method"
+//A. Child7 x=200  method() -> @"Child Method"
 class Parent7  extends Object{
 	   int x = 100;
 	   public Parent7() { super(); }
@@ -14,13 +24,13 @@ class Parent7  extends Object{
 public class PolyEx005 {
 	public static void main(String[] args) {
 	      Parent7 p = new Child7();     
-	      // Q3.  Parent7 p   보장하는 범위   
-	      // Q4. 인스턴스화 했을때 사용가능한 범위 : new Child7()  
+	      // Q3.  Parent7 p   보장하는 범위   A. x-100, @method() "Child Method"
+	      // Q4. 인스턴스화 했을때 사용가능한 범위 : new Child7()  A. x = 200 @method() → "Child Method" - x = 100 , -------
 	      
 	                            Child7 c = new Child7();     
-	      System.out.println("p.x = " + p.x);  // Q5. 출력되는 내용   
-	      p.method();  // Q6. 출력되는 내용     
-	      System.out.println("c.x = " + c.x);   // Q7. 출력되는 내용  
-	      c.method();   // Q8. 출력되는 내용   
+	      System.out.println("p.x = " + p.x);  // Q5. 출력되는 내용  A. p.x = 100
+	      p.method();  // Q6. 출력되는 내용       					  A. Child Method
+	      System.out.println("c.x = " + c.x);   // Q7. 출력되는 내용 A. c.x = 200
+	      c.method();   // Q8. 출력되는 내용   						  A. Child Method
 	   }
 }
