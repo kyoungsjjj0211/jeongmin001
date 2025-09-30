ff530db3bcd36848f67d370a2b634c62bc55c97f
kyoungsjjj0211 <kyoungsjjj0513@gmail.com>
2025-09-30 17:57:10 +0900

    back

--- track003_java/java2025/src/com/company/java012_Ex/PolyEx005.java
+++ track003_java/java2025/src/com/company/java012_Ex/PolyEx005.java
@@ -4,7 +4,7 @@ Object
   ↑
 Parent7  int x=100, method() -> "Parent Method"
   ↑
-Child7 int x= 200, method() -> "ChildMethod"
+Child7 	 int x= 200, method() -> "ChildMethod"
 */
 //Q1. 상속도 그리기
 //Q2. 각클래스에서 사용할수있는 멤버변수/멤버메서드
@@ -21,6 +21,8 @@ class Parent7  extends Object{
 	   @Override  void method() { System.out.println("Child Method"); }
 	}
 	
+	
+	
 public class PolyEx005 {
 	public static void main(String[] args) {
 	      Parent7 p = new Child7();     
@@ -32,5 +34,10 @@ public class PolyEx005 {
 	      p.method();  // Q6. 출력되는 내용       					  A. Child Method
 	      System.out.println("c.x = " + c.x);   // Q7. 출력되는 내용 A. c.x = 200
 	      c.method();   // Q8. 출력되는 내용   						  A. Child Method
+	      //Q9. main 에서 부모메서드 호출하기 ? ((Parent7)p).method(); //x 어버라이딩 된 메서드를 직접호출하는건 불가능
+	      ((Parent7)p).method();//x 오버라이딩 된 메서드를 직접호출하는건 불가능
+	      c.papaMethod(); // 자식에서 부모호출가능
+	      ((Child7)p).papaMethod(); //타입캐스팅 - 부모가 자식메서드 호출가능 ( 자식생성자 호출시)
 	   }
 }
+
