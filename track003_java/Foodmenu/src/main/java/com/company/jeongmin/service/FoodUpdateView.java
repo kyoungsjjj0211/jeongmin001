ffa84ce04ae35df0f69a30a1450ede6cd30de56a
kyoungsjjj0211 <kyoungsjjj0513@gmail.com>
2025-11-05 18:21:40 +0900

    포트폴리오

--- track003_java/Foodmenu/src/main/java/com/company/jeongmin/service/FoodUpdateView.java
+++ track003_java/Foodmenu/src/main/java/com/company/jeongmin/service/FoodUpdateView.java
@@ -1,5 +1,25 @@
 package com.company.jeongmin.service;
 
-public class FoodUpdateView {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.company.jeongmin.Dao_Dto.FoodDao;
+
+public class FoodUpdateView implements FoodService{
+
+	@Override
+	public void exec(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		int id = Integer.parseInt(request.getParameter("id"));
+		
+		FoodDao dao = new FoodDao();
+		
+		request.setAttribute("dto", dao.select(id));
+	}
+
+	
 }
