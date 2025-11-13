6034f89e490f68a2a049dbec451f2fec3f9672c8
kyoungsjjj0211 <kyoungsjjj0513@gmail.com>
2025-11-13 17:45:04 +0900

    back

---
+++ track006_spring/spring005/src/main/java/com/thejoa703/controller/BasicController.java
@@ -0,0 +1,24 @@
+package com.thejoa703.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BasicController {
+
+	@RequestMapping("/basic.do")
+	public String basic(Model model) {
+		//1. 처리해결사-service
+		model.addAttribute("result" , "Hello");
+		return "basic.jsp"; 		//2. 해당view  --error 없다고 나올것임.
+	}
+	
+	@RequestMapping("/basic2.do")
+	public String basic2(Model model) {
+		//1. 처리해결사-service
+		model.addAttribute("result" , "Hello");
+		return "basic"; 		//2. 해당view   /view/+ basic(파일명) + .jsp
+	}
+	
+}
